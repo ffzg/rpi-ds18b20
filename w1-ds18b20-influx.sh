ff81f8124933177cf0b3fd305295cf0c9182b968
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-25 20:19:54 +0200

    support older 4.1 kernels without temperature in /sys/bus/w1

--- w1-ds18b20-influx.sh
+++ w1-ds18b20-influx.sh
@@ -5,8 +5,8 @@ influx=/dev/shm/temp.influx
 
 :> $influx
 
-ls /sys/bus/w1/devices/w1_bus_master*/*-*/temperature | while read path ; do
-	temp=$( cat $path | awk '{ printf "%.3f\n", $1 / 1000 }' )
+ls /sys/bus/w1/devices/w1_bus_master*/*-*/w1_slave | while read path ; do
+	temp=$( cat $path | grep t= | cut -d= -f2  | awk '{ printf "%.3f\n", $1 / 1000 }' )
 	id=$( echo $path | cut -d/ -f 7 )
 	echo "ac_temp,dc=a125,sensor=$id temperature=$temp" >> $influx
 	echo $temp > /dev/shm/ds18b20.$id
