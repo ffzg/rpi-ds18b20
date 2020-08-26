9d69f0ad121275040f1217fa939234da585455a2
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-26 14:19:43 +0200

    added name of sensor

--- w1-ds18b20.sh
+++ w1-ds18b20.sh
@@ -5,7 +5,8 @@ ls /sys/bus/w1/devices/w1_bus_master*/*-*/w1_slave | while read path ; do
 	if [ $temp != 85000 ] ; then # 85 is error
 		temp=$( echo $temp | awk '{ printf "%.3f\n", $1 / 1000 }' )
 		id=$( echo $path | cut -d/ -f 7 )
-		echo -n "$id "
+		name=$( grep "^$id" id2name.txt | cut -d' ' -f2 )
+		echo -n "$id $name "
 		echo $temp | tee /dev/shm/ds18b20.$id
 	fi
 done
