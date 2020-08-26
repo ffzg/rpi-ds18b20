b0e534ad1025555ae3d2d5f8321165a6dbd4e8aa
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-26 12:31:00 +0200

    shell doesn't support floats so use int values for test

--- w1-ds18b20-influx.sh
+++ w1-ds18b20-influx.sh
@@ -6,8 +6,9 @@ influx=/dev/shm/temp.influx
 :> $influx
 
 ls /sys/bus/w1/devices/w1_bus_master*/*-*/w1_slave | while read path ; do
-	temp=$( cat $path | grep t= | cut -d= -f2  | awk '{ printf "%.3f\n", $1 / 1000 }' )
-	if [ $temp != 85 ] ; then # 85 is error
+	temp=$( cat $path | grep t= | cut -d= -f2 )
+	if [ $temp -ne 85000 ] ; then # 85 is error
+		temp=$( echo $temp | awk '{ printf "%.3f\n", $1 / 1000 }' )
 		id=$( echo $path | cut -d/ -f 7 )
 		echo "ac_temp,dc=a125,sensor=$id temperature=$temp" >> $influx
 		echo $temp > /dev/shm/ds18b20.$id
