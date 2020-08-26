cf4f3da7ab035641448eaa6e11048df6a55a71b2
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-26 14:34:24 +0200

    check if we got temperature from kernel

--- w1-ds18b20-influx.sh
+++ w1-ds18b20-influx.sh
@@ -7,7 +7,7 @@ influx=/dev/shm/temp.influx
 
 ls /sys/bus/w1/devices/w1_bus_master*/*-*/w1_slave | while read path ; do
 	temp=$( cat $path | grep t= | cut -d= -f2 )
-	if [ $temp -ne 85000 ] ; then # 85 is error
+	if [ ! -z "$temp" -a $temp -ne 85000 ] ; then # 85 is error
 		temp=$( echo $temp | awk '{ printf "%.3f\n", $1 / 1000 }' )
 		id=$( echo $path | cut -d/ -f 7 )
 		name=$( grep "^$id" id2name.txt | cut -d' ' -f2 )
