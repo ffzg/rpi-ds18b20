53a8274be4b4133010e1143050c455a11890fea4
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-26 08:09:02 +0200

    temperature 85 is error condition

--- w1-ds18b20-influx.sh
+++ w1-ds18b20-influx.sh
@@ -7,9 +7,11 @@ influx=/dev/shm/temp.influx
 
 ls /sys/bus/w1/devices/w1_bus_master*/*-*/w1_slave | while read path ; do
 	temp=$( cat $path | grep t= | cut -d= -f2  | awk '{ printf "%.3f\n", $1 / 1000 }' )
-	id=$( echo $path | cut -d/ -f 7 )
-	echo "ac_temp,dc=a125,sensor=$id temperature=$temp" >> $influx
-	echo $temp > /dev/shm/ds18b20.$id
+	if [ $temp != 85 ] ; then # 85 is error
+		id=$( echo $path | cut -d/ -f 7 )
+		echo "ac_temp,dc=a125,sensor=$id temperature=$temp" >> $influx
+		echo $temp > /dev/shm/ds18b20.$id
+	fi
 done
 
 #curl --silent -XPOST "$influx_url" --data-binary "@$influx"
