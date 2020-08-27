8100bb3c0af94e00a9759c49d450089f888b3e1f
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-27 12:59:51 +0200

    added DC env var for dc tag in influx (room number)

--- w1-ds18b20-influx.sh
+++ w1-ds18b20-influx.sh
@@ -2,6 +2,7 @@
 
 influx_url="http://10.60.0.92:8086/write?consistency=any&db=ffzg"
 influx=/dev/shm/temp.influx
+test -z "$DC" && DC=a125
 
 :> $influx
 
@@ -11,7 +12,7 @@ ls /sys/bus/w1/devices/w1_bus_master*/*-*/w1_slave | while read path ; do
 		temp=$( echo $temp | awk '{ printf "%.3f\n", $1 / 1000 }' )
 		id=$( echo $path | cut -d/ -f 7 )
 		name=$( grep "^$id" id2name.txt | cut -d' ' -f2 )
-		echo "ac_temp,dc=a125,sensor=$id,name=$name temperature=$temp" >> $influx
+		echo "ac_temp,dc=$DC,sensor=$id,name=$name temperature=$temp" >> $influx
 		echo $temp > /dev/shm/ds18b20.$id
 		echo $temp > /dev/shm/ds18b20.name.$name
 	fi
